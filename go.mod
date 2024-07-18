52d5a555a96b131d58c1b5245d785d282ee5e263
Eric Caverly <eric@ericc.ninja>
2024-07-17 19:48:47 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EricCaverly/simpletui
+
+go 1.22.5
---
+++ simpletui.go
@@ -0,0 +1,7 @@
+package simpletui
+
+import "fmt"
+
+func Label(x int, y int, text string) {
+    fmt.Printf("`%s`\n", text);
+}
